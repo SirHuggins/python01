19ee713e184eb42c0e4c92661cc4c38f7bb934a1
SirHuggins <jahbarihuggins16@gmail.com>
2016-04-19 15:06:01 -0400

    Assigment_3.1_Jahbari_Huggins.py

---
+++ Assignments/Assigment_3.1_Jahbari_Huggins.py
@@ -0,0 +1,12 @@
+hrs = float(raw_input("how many hours do you work: ")
+rate = float(raw_input("what is your rate: ")
+
+#under or equal 40 hours , basic pay
+if hrs 
+pay = hrs * rate
+
+#if it's greater than 40 , basic pay for the 40 hours + hours x pay x 1.5
+
+
+print pay
+
